f23f0f7eb9c65db9ddef6c6a13500788f92fa157
Caelan Priddy <caelanpriddy@gmail.com>
2022-10-12 11:38:29 -0400

    Tracking

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 
 int main(){
     FILE * fileptr;
-    FILE * FileLength; 
+    long FileLength; 
     fileptr = fopen("unesco.raw","rb");
     fseek(fileptr, 0, SEEK_END);
     FileLength = ftell(fileptr);
     rewind(fileptr);
+
+    //buffers 1 & 2
+    //create an empty
+    
 
 
     return 0;
