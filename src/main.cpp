715a4c20b03250dd3ab8933d4fb4699c0e727547
Caelan Priddy <caelanpriddy@gmail.com>
2022-10-12 17:44:00 -0400

    255 - a for reverse Complement of 2's instance

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(){
             a = buffer[i][j];
             b = buffer[i][j];
             c = buffer[i][j];
-            outimageReverse[i][j] = a - 255;
+            outimageReverse[i][j] = 255 - a;
             outimageAddLess[i][j] = b + 20;
             outimageAddMore[i][j] = c + 128;
             //outimage[i][j] = a;
@@ -39,59 +39,5 @@ int main(){
     fclose(fp3);
     fclose(fp4);
     
-    /*
-    FILE * fileptr;
-    FILE * writeptr;
-    long FileLength;
-    char * buffer;
-    char * bufferRepeat;
-      //long fl = ftell(fp);
-    //FILE * fp2 = fopen("unesco.raw","rb");
-    //fread(buffer2, W*H, 1, fp);
-    for(i = 0; i < W; i++){
-        for(j = 0; j < H; j++){
-            a = buffer[i][j];
-            outimage[i][j] = a + 20;
-    }
-    }
-    
-    
-    fileptr = fopen("unesco.raw","rb");
-   // fseek(fileptr, 0, SEEK_END);
-    FileLength = ftell(fileptr);
-    rewind(fileptr);
-    buffer = (char*)malloc(sizeof(char)*FileLength);
-    bufferRepeat = (char*)malloc(sizeof(char)*FileLength);
-    fread(buffer,sizeof(buffer),1,fileptr);
-    fwrite(buffer,sizeof(buffer),1,writeptr);
-
+    return 0;
 }
-Image& Image::flipX() {
-	uint8_t tmp[4];
-	uint8_t* px1;
-	uint8_t* px2;
-	for(int y = 0;y < h;++y) {
-		for(int x = 0;x < w/2;++x) {
-			px1 = &data[(x + y * w) * channels];
-			px2 = &data[((w - 1 - x) + y * w) * channels];
-			
-			memcpy(tmp, px1, channels);
-			memcpy(px1, px2, channels);
-			memcpy(px2, tmp, channels);
-		}
-	}
-	return *this;
-}
-Image& Image::flipY(){
-
-    return *this;
-}
-
-/*
-void swap_int (int *a, int *b)
-{
-    int temp = *b;
-    *b = *a;
-    *a = temp;
-}
-*/
